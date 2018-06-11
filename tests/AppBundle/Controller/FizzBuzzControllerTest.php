b7480f604e550a15bbac6532e00250c3b2e04d82
Xavi <xavi@geekshubs.com>
2018-06-11 19:39:49 +0200

    [RED] Create Test, for CorrectReplacement method

--- tests/AppBundle/Controller/FizzBuzzControllerTest.php
+++ tests/AppBundle/Controller/FizzBuzzControllerTest.php
@@ -77,4 +77,25 @@ class FizzBuzzControllerTest extends WebTestCase
             [60, true],
         ];
     }
+
+    /**
+     * @test
+     * @dataProvider listValuesProvider
+     */
+    public function itShouldGenerateTheCorrectReplacementForEachNumber($value, $expected)
+    {
+        $this->assertEquals($expected, FizzBuzz::getReplacement($value));
+    }
+
+    public function listValuesProvider()
+    {
+        return [
+            [1,1],
+            [2,2],
+            [3,'Fizz'],
+            [4,4],
+            [5,'Buzz'],
+            [15,'FizzBuzz'],
+        ];
+    }
 }
