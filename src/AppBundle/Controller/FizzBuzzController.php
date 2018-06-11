d80afddbb410454b3c2aad1f1d8987ca3770a715
Xavi <xavi@geekshubs.com>
2018-06-11 19:27:56 +0200

    [Refactor] Remove code Smells, Magic Numbers

--- src/AppBundle/Controller/FizzBuzzController.php
+++ src/AppBundle/Controller/FizzBuzzController.php
@@ -7,14 +7,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class FizzBuzzController extends Controller
 {
+    const FIZZ_FACTOR =3;
+    const BUZZ_FACTOR = 5;
+
     public function isFizz(int $value):int
     {
-        return 0=== $value % 3;
+        return 0=== $value % self::FIZZ_FACTOR;
     }
 
     public function isBuzz(int $value):int
     {
-        return 0=== $value % 5;
+        return 0=== $value % self::BUZZ_FACTOR;
     }
 
     public function isFizzBuzz(int $value):int
